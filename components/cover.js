117d7cf660c9c3ffa67b64d6eb922ebf12f0a177
Boris Veltman <baveltman@gmail.com>
2016-08-21 21:02:48 -0700

    better css for cover

--- components/cover.js
+++ components/cover.js
@@ -16,15 +16,14 @@ export default class MarketingHeader extends React.Component {
 		return (
       <div>
         <div className="cover-copy">
+					image
           <div className="cover-heading">
-            What Are Customers Saying About Your Business?
+            <h1>What Are Customers Saying About Your Business?</h1>
           </div>
           <div className="cover-input">
             <input type="text" placeholder="Enter Business Name or Address..." />
           </div>
         </div>
-  			<div className="cover-image">
-  			</div>
       </div>
 			);
 	}
